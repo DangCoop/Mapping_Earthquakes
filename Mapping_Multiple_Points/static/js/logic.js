aa27b8bc6c05a56784ece7c139a0c3521eaea4e2
Denis Antonov <antonov.reca@gmail.com>
2022-09-09 18:52:54 -0400

    Add New Version of logic.js

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -2,47 +2,22 @@
 console.log("working");
 
 //Create the map object with a center and zoom level.
-//let map = L.map('mapid').setView([40.7, -94.5], 4);
+let map = L.map('mapid').setView([34.0522, -118.2437], 14);
 
-// An array containing each city's location, state, and population.
-let cities = [{
-    location: [40.7128, -74.0059],
-    city: "New York City",
-    state: "NY",
-    population: 8398748
-},
-{
-    location: [41.8781, -87.6298],
-    city: "Chicago",
-    state: "IL",
-    population: 2705994
-},
-{
-    location: [29.7604, -95.3698],
-    city: "Houston",
-    state: "TX",
-    population: 2325502
-},
-{
-    location: [34.0522, -118.2437],
-    city: "Los Angeles",
-    state: "CA",
-    population: 3990456
-},
-{
-    location: [33.4484, -112.0740],
-    city: "Phoenix",
-    state: "AZ",
-    population: 1660272
-}
-];
+//  Add a marker to the map for Los Angeles, California.
+//let marker = L.marker([34.0522, -118.2437]).addTo(map);
 
-// Loop through the cities array and create one marker for each city.
-cities.forEach(function(city) {
-    console.log(city)
-});
+//Add a merker to the map for LA, California
+    // L.circle([34.0522, -118.2437], {
+    //     radius: 100
+    // }).addTo(map);
 
-
+//Add a circle to the map
+L.circleMarker([34.0522, -118.2437],{
+    radius: 300,
+    color: "black",
+    fillColor: '#ffffa1'
+}).addTo(map);
 
 // We create the tile layer that will be the background of our map.
 // let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -58,7 +33,7 @@ cities.forEach(function(city) {
 
 //New Version of tile layer.
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
