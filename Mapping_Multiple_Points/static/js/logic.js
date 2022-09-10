49edddb4795d8361b794af664748934a841b6320
Denis Antonov <antonov.reca@gmail.com>
2022-09-10 15:48:11 -0400

    Add Mulpiple circleMarkers with Popup Menu Version3

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -2,22 +2,52 @@
 console.log("working");
 
 //Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([34.0522, -118.2437], 14);
+let map = L.map('mapid').setView([34.0522, -118.2437], 4);
+
+
+// Get data from cities.js.
+let cityData = cities;
+
+
+// Loop through the cities array and create one marker for each city.
+// cityData.forEach(function(city) {
+//     console.log(city)
+//     L.marker(city.location)
+//     .addTo(map);
+// });
+
+//Add to the loop - PopUp menu with city, state and population of each city on the map
+//wiht metod "tpLocaleString" formating population with the thousands separator.
+//Change 'marker' to a 'circleMarker' and assing the "radius" key to the population using (city.population)
+//radius: city.population/100000 - correct radius size for the map
+cityData.forEach(function(city) {
+    console.log(city)
+    L.circleMarker(city.location, {
+        radius: city.population/200000,
+        color: "orange",
+        weight: 4
+    })
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+    .addTo(map);
+});
+
+
+
 
 //  Add a marker to the map for Los Angeles, California.
 //let marker = L.marker([34.0522, -118.2437]).addTo(map);
 
-//Add a merker to the map for LA, California
+//Add a marker to the map for LA, California
     // L.circle([34.0522, -118.2437], {
     //     radius: 100
     // }).addTo(map);
 
 //Add a circle to the map
-L.circleMarker([34.0522, -118.2437],{
-    radius: 300,
-    color: "black",
-    fillColor: '#ffffa1'
-}).addTo(map);
+// L.circleMarker([34.0522, -118.2437],{
+//     radius: 300,
+//     color: "black",
+//     fillColor: '#ffffa1'
+// }).addTo(map);
 
 // We create the tile layer that will be the background of our map.
 // let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
