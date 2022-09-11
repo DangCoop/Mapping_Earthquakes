7d7fa4102db259c6bdc080b77a077beb6bb1a2bb
Denis Antonov <antonov.reca@gmail.com>
2022-09-11 17:55:07 -0400

    Add Airlines destinations with popup menu

--- Mapping_GeoJSON_Linestrings/static/js/logic.js
+++ Mapping_GeoJSON_Linestrings/static/js/logic.js
@@ -1,103 +1,14 @@
-//Add console.log to check to see if our code is working
-//console.log("working");
+//15.5.5 GeoJSON LineString type objects.
 
-//Create the map object with a center and zoom level.
-//let map = L.map('mapid').setView([37.5, -122.5], 10);
-
-
- // Add GeoJSON data.
-// let sanFranAirport =
-// {"type":"FeatureCollection","features":[{
-//     "type":"Feature",
-//     "properties":{
-//         "id":"3469",
-//         "name":"San Francisco International Airport",
-//         "city":"San Francisco",
-//         "country":"United States",
-//         "faa":"SFO",
-//         "icao":"KSFO",
-//         "alt":"13",
-//         "tz-offset":"-8",
-//         "dst":"A",
-//         "tz":"America/Los_Angeles"},
-//         "geometry":{
-//             "type":"Point",
-//             "coordinates":[-122.375,37.61899948120117]}}
-// ]};
-
-//Version 1 to add Popup menu with "pointToLayer()"
-// Grabbing our GeoJSON data.
-// L.geoJSON(sanFranAirport, {
-    // We turn each feature into a marker on the map.
-//     pointToLayer: function(feature, latlng) {
-//     console.log(feature);
-//     return L.marker(latlng)
-//     .bindPopup("<h2>" + feature.properties.name + "</h2> <hr> <h3>" + feature.properties.city + ", " + feature.properties.country + "</h3>")
-//     }
-
-// }).addTo(map);
-
-//Version 2 to add Popup menu with "onEachFeature" function
-// L.geoJSON(sanFranAirport, {
-//     onEachFeature: function(feature, layer) {
-//         console.log(layer);
-//         layer.bindPopup("<h2>Airport code: " + feature.properties.faa + "</h2> <hr> <h3>Airport name:" + feature.properties.name + "</h3>");
-//     }
-// }).addTo(map);
-
-// Grabbing our GeoJSON data.
-//L.geoJSON(sanFranAirport).addTo(map);
-
-// let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-// attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-//     maxZoom: 18,
-//     accessToken: API_KEY
-// });
-
-// Then we add our 'graymap' tile layer to the map.
-//streets.addTo(map); 
-
-//15.5.3 New Challenge Multiple Markers with popup menu - d3.json>
-
-// //Create the map object with a center and zoom level.
-// let map = L.map('mapid').setView([30, 30], 2);
-
-// //We create the tile layerthat will be the background of our map.
-// let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-// attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-//     maxZoom: 18,
-//     accessToken: API_KEY
-// });
-
-// // Then we add our 'graymap' tile layer to the map.
-// streets.addTo(map); 
-
-// // Accessing the airport GeoJSON URL
-// let airportData ="https://raw.githubusercontent.com/DangCoop/Mapping_Earthquakes/main/majorAirports.json"
-
-// // Grabbing our GeoJSON data.
-// d3.json(airportData).then(function(data) {
-//     console.log(data);
-//     // Creating a GeoJSON layer with the retrieved data.
-// L.geoJSON(data, {
-//     onEachFeature: function(feature, layer) {
-//     console.log(layer);
-//     layer.bindPopup("<h2>Airport code: " + feature.properties.faa + "</h2> <hr> <h3>Airport name:" + feature.properties.name + "</h3>");
-//     }
-// }).addTo(map);
-// });
-
-//15.5.4 Adding another map.
-
-//We create the tile layerthat will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+//We create the light view tile layerthat will be the background of our map. (change from light-v10 to navigation-day-v1 map)
+let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/navigation-day-v1/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
 });
 
-// We create the dark view tile layer that will be an option for our map.
-let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+// We create the dark view tile layer that will be an option for our map.(change from dark-v10 to navigation-night-v1 map)
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/navigation-night-v1/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -105,15 +16,15 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 
 // Create a base layer that holds both maps.
 let baseMaps = {
-    Street: streets,
+    Light: light,
     Dark: dark
 };
 
-//Create the map object with a center and zoom level.
+//Create the map object with a center and zoom level + default layer in the map object
 let map = L.map('mapid', {
-    center:[30, 30],
+    center:[44.0, -80.0],
     zoom: 2,
-    layers: [streets]
+    layers: [light]
 });
 
 //Pass our map layers into our layer control and add the layer control to the map.
@@ -122,17 +33,25 @@ L.control.layers(baseMaps).addTo(map);
 // Then we add our 'graymap' tile layer to the map.
 //streets.addTo(map); 
 
-// Accessing the airport GeoJSON URL
-let airportData ="https://raw.githubusercontent.com/DangCoop/Mapping_Earthquakes/main/majorAirports.json"
+// Accessing the Toronto airlines routes GeoJSON URL
+let torontoData ="https://raw.githubusercontent.com/DangCoop/Mapping_Earthquakes/main/torontoRoutes.json"
+
+// Create a style for the lines.
+let myStyle = {
+    color: "#ffffa1",
+    weight: 2
+}
 
 // Grabbing our GeoJSON data.
-d3.json(airportData).then(function(data) {
+d3.json(torontoData).then(function(data) {
     console.log(data);
-    // Creating a GeoJSON layer with the retrieved data.
-L.geoJSON(data, {
-    onEachFeature: function(feature, layer) {
-    console.log(layer);
-    layer.bindPopup("<h2>Airport code: " + feature.properties.faa + "</h2> <hr> <h3>Airport name: " + feature.properties.name + "</h3>");
-    }
-}).addTo(map);
+    //Creating a GeoJSON layer with the retrieved data.
+    L.geoJSON(data,{
+        style: myStyle,
+        //We turn each feature into a marker on the map.
+        onEachFeature: function(feature, layer) {
+        console.log(layer);
+        layer.bindPopup("<h2>Airline: " + feature.properties.airline + "</h2> <hr> <h3>Destination: " + feature.properties.dst + "</h3>");
+        }
+    }).addTo(map);
 });
