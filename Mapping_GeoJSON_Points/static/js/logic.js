83cdfc10001bc271cd64beb9a525101a0b204381
Denis Antonov <antonov.reca@gmail.com>
2022-09-11 15:14:27 -0400

    Add Map Layers Street/Dark

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -57,10 +57,37 @@
 // Then we add our 'graymap' tile layer to the map.
 //streets.addTo(map); 
 
-//New Challenge
+//15.5.3 New Challenge Multiple Markers with popup menu - d3.json>
 
-//Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([30, 30], 2);
+// //Create the map object with a center and zoom level.
+// let map = L.map('mapid').setView([30, 30], 2);
+
+// //We create the tile layerthat will be the background of our map.
+// let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+// attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+//     maxZoom: 18,
+//     accessToken: API_KEY
+// });
+
+// // Then we add our 'graymap' tile layer to the map.
+// streets.addTo(map); 
+
+// // Accessing the airport GeoJSON URL
+// let airportData ="https://raw.githubusercontent.com/DangCoop/Mapping_Earthquakes/main/majorAirports.json"
+
+// // Grabbing our GeoJSON data.
+// d3.json(airportData).then(function(data) {
+//     console.log(data);
+//     // Creating a GeoJSON layer with the retrieved data.
+// L.geoJSON(data, {
+//     onEachFeature: function(feature, layer) {
+//     console.log(layer);
+//     layer.bindPopup("<h2>Airport code: " + feature.properties.faa + "</h2> <hr> <h3>Airport name:" + feature.properties.name + "</h3>");
+//     }
+// }).addTo(map);
+// });
+
+//15.5.4 Adding another map.
 
 //We create the tile layerthat will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -69,8 +96,31 @@ attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStree
     accessToken: API_KEY
 });
 
+// We create the dark view tile layer that will be an option for our map.
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Create a base layer that holds both maps.
+let baseMaps = {
+    Street: streets,
+    Dark: dark
+};
+
+//Create the map object with a center and zoom level.
+let map = L.map('mapid', {
+    center:[30, 30],
+    zoom: 2,
+    layers: [streets]
+});
+
+//Pass our map layers into our layer control and add the layer control to the map.
+L.control.layers(baseMaps).addTo(map);
+
 // Then we add our 'graymap' tile layer to the map.
-streets.addTo(map); 
+//streets.addTo(map); 
 
 // Accessing the airport GeoJSON URL
 let airportData ="https://raw.githubusercontent.com/DangCoop/Mapping_Earthquakes/main/majorAirports.json"
@@ -82,7 +132,7 @@ d3.json(airportData).then(function(data) {
 L.geoJSON(data, {
     onEachFeature: function(feature, layer) {
     console.log(layer);
-    layer.bindPopup("<h2>Airport code: " + feature.properties.faa + "</h2> <hr> <h3>Airport name:" + feature.properties.name + "</h3>");
+    layer.bindPopup("<h2>Airport code: " + feature.properties.faa + "</h2> <hr> <h3>Airport name: " + feature.properties.name + "</h3>");
     }
 }).addTo(map);
 });
