972d2f9e3113b75b11ad595b6966d3fe744189b6
Denis Antonov <antonov.reca@gmail.com>
2022-09-11 23:25:49 -0400

    Version4 of Code add Earthquakes Layer to the map

--- Earthquakes_past7days/static/js/logicStep4.js
+++ Earthquakes_past7days/static/js/logicStep4.js
@@ -20,6 +20,16 @@ let baseMaps = {
     "Satellite Streets": satelliteStreets
 };
 
+// Create the earthquake layer for our map.
+let earthquakes = new L.layerGroup();
+
+// We define an object that contains the overlays.
+// This overlay will be visible all the time.
+let overlays = {
+    Earthquakes: earthquakes
+};
+
+
 //Create the map object with a center and zoom level + default layer in the map object
 let map = L.map('mapid', {
     center:[39.5, -98.5],
@@ -28,7 +38,7 @@ let map = L.map('mapid', {
 });
 
 //Pass our map layers into our layer control and add the layer control to the map.
-L.control.layers(baseMaps).addTo(map);
+L.control.layers(baseMaps, overlays).addTo(map);
 
 // Accessing the Toronto airlines routes GeoJSON URL
 let SevenDEarthquake ="https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
@@ -97,6 +107,8 @@ d3.json(SevenDEarthquake).then(function(data) {
         onEachFeature: function(feature, layer) {
             layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
         }
-    }).addTo(map);
+    }).addTo(earthquakes);
     
+    //Then we add the earthquake layer to uor map.
+    earthquakes.addTo(map);
 });
